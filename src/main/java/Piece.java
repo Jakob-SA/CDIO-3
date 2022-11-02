35f43793fd47a27d1d82b9fe13e170aaa8e8d317
Esben <esbenskov@live.dk>
2022-11-02 15:51:59 +0100

    Piece class version 1 added

---
+++ src/main/java/Piece.java
@@ -0,0 +1,23 @@
+public class Piece {
+    private int location;
+    private String color;
+
+
+
+    public int addLocation(int dieSum)
+    {
+        location+=dieSum;
+        return location;
+    }
+    public int getLocation(){
+        return location;
+    }
+
+    public void setLocation(int field)
+    {
+        location = field;
+    }
+    public String toString(){
+        return ""+location;
+    }
+}
