d651f673e7cf758cec71de19016e324c68ed57bb
Jakob-SA <agergaardskovjakob@gmail.com>
2022-11-02 16:23:36 +0100

    MaxFaceValue set to static
    Took 2 minutes

--- src/main/java/org/example/Die.java
+++ src/main/java/org/example/Die.java
@@ -1,7 +1,8 @@
 package org.example;
 
 public class Die {
-    private int faceValue, maxFaceValue;
+    private int faceValue;
+    private static int maxFaceValue;
     public Die (int dieSide) {maxFaceValue = dieSide;}
     public void roll () {
         faceValue = (int)(Math.random()*maxFaceValue)+1;
