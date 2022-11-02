2a22dd3d2196416f4cb7a9cdd471750b6c006ad7
philipMF <philipmuff@gmail.com>
2022-11-02 15:51:53 +0100

    Created 'Players' class

---
+++ src/main/java/org/example/Players.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Players {
+    //private Account bank;
+    private String name;
+    private int Player;
+
+    private int Player(int x){
+        return 0;
+    }
+
+    private void Piece(int x){
+    }
+}
